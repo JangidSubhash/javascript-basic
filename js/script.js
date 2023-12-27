d0a0d1b032eff9c13b3f66ac087a16e8b58109ed
JangidSubhash <subhash.jangid@crystaltechesolutions.com>
2023-12-27 18:49:04 +0530

    fat arrow

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function myFunctionA(m){
+    console.log("I am "+m+" and I am Full Stack Developer");
+}
+
+myFunctionA('Subhash Jangid');
+
+let myFunctionB = (subh)=>{
+    console.log(subh+" Working in Accenture Company");
+}
+
+myFunctionB('Subhash Jangid');
